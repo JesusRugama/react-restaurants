a9ac8a26272748030520c75102b07103ac75fa73
Jesus <ajrugama@gmail.com>
2020-12-07 13:10:41 -0600

    Router added, progress in project structure Adds firebase, node-sass and react-router Adds initial testing components Adds empty components to define a basic project structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import Reservations from './pages/reservations/reservations.component';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path='/' component={Reservations} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/reservations/reservations.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Reservations = () => (
+  <h1>Test</h1>
+);
+
+export default Reservations;
