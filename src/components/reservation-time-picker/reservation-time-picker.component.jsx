3268ee517aca4148e39f04b9ebbecfebfc03bc10
Jesus <ajrugama@gmail.com>
2021-01-02 15:35:57 -0600

    RES-34 Remove used times from time select when creating/updating reservations

--- src/components/reservation-time-picker/reservation-time-picker.component.jsx
+++ src/components/reservation-time-picker/reservation-time-picker.component.jsx
@@ -1,11 +1,47 @@
-const TimePicker = ({ name, value, label, handleChange, ...props }) => {
+import { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectReservations } from "../../redux/reservation/reservation.selectors";
+import { getReservationsStart } from "../../redux/reservation/reservation.actions";
+
+const TimePicker = ({
+  reservations,
+  name,
+  value,
+  label,
+  handleChange,
+  ...props
+}) => {
+ /**
+ * Read existing reservations and add used hours in an array to prevent using the same hour for two reservations
+ */
+  const [usedHours, setUsedHours] = useState([]);
+  useEffect(() => {
+    let startOfDay = new Date(value.getTime());
+    startOfDay.setHours(0, 0, 0, 0);
+    let endOfDay = new Date(value.getTime());
+    endOfDay.setHours(23, 59, 59, 999);
+
+    const dayReservations = reservations
+      .filter(
+        (reservation) => {
+            let date = reservation.reservationDate.toDate();
+            return (date >= startOfDay && date <= endOfDay);
+        }
+      );
+
+    const hours = dayReservations.map((reservation) => reservation.reservationDate.toDate().getHours());
+    setUsedHours(hours);
+  }, [reservations, value]);
+
   const handleSelectChange = (event) => {
     value.setHours(event.target.value, 0, 0, 0);
     handleChange({ target: { name, value } });
   };
 
-  const availableTimes = Array.from(Array(24).keys()).filter(
-    (v) => v <= 22 && v >= 9
+  const availableHours = Array.from(Array(24).keys()).filter(
+    (hour) => hour <= 22 && hour >= 9
   );
 
   const hourToAmPm = (hourNumber) => {
@@ -20,13 +56,25 @@ const TimePicker = ({ name, value, label, handleChange, ...props }) => {
 
   return (
     <select name={name} onChange={handleSelectChange} value={value.getHours()}>
-      {availableTimes.map((time, i) => (
-        <option value={time} key={i}>
-          {hourToAmPm(time)}
-        </option>
-      ))}
+      {availableHours
+        .filter(
+          (hour) => (value.getHours() === hour) || (-1 === usedHours.findIndex((usedHour) => hour === usedHour))
+        )
+        .map((time, i) => (
+          <option value={time} key={i}>
+            {hourToAmPm(time)}
+          </option>
+        ))}
     </select>
   );
 };
 
-export default TimePicker;
+const mapStateToProps = createStructuredSelector({
+  reservations: selectReservations,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getReservationsStart: (payload) => dispatch(getReservationsStart(payload)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TimePicker);
